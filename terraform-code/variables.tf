c8f4b07f47ae6fbc8ad8ed5b73797ea456bf8d97
Aksun <Yusuf.Aksun@thomsonreuters.com>
2020-10-24 09:39:30 +0100

    vpc terraform

--- terraform-code/variables.tf
+++ terraform-code/variables.tf
@@ -22,7 +22,7 @@ variable "public_subnets" {
 variable "private_subnets" {
   description = "List of CIDRs for private subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default     = ["10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "database_subnets" {
